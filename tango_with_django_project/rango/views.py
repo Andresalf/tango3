4978fdaf084f007424b7ced3cef076ded5095669
Andres Garcia <agarcia@localhost.localdomain>
2017-03-06 21:22:57 -0600

    About page and links

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(r'Rango says hey there partner!<br><a href="about">About</a>')
+
+def about(request):
+    return HttpResponse(r'Rango says here is the about page.<br><a href="/rango/">Home</a>')
